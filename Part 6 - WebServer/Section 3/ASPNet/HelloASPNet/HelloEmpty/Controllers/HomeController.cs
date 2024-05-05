331793bed2be07416acf9f74b69628feb9b0f371
9kyo-hwang <ryghkd507@naver.com>
2024-05-06 00:29:22 +0900

    [FEATURE] Hello MVC #1 완료

---
+++ Part 6 - WebServer/Section 3/ASPNet/HelloASPNet/HelloEmpty/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using HelloEmpty.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloEmpty.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        HelloMessage msg = new HelloMessage
+        {
+            Message = "Welcome to ASP.Net MVC!"
+        };
+
+        ViewBag.Notification = "Input message and click to submit";
+        
+        return View(msg);
+    }
+}
